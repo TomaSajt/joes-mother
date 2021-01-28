fbe11d10cf16fa72488338ec01bfb3676c8d3387
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2021-01-28 18:01:41 +0100

    pause command exists????

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 ﻿const Discord = require('discord.js');
 const config = require('./config.json');
 var crashing = false;
-var pause = false;
+const pauseUnpauseCommand = require('./commands/special/sync/pause unpause.js');
 var normalCommands = [
     require('./commands/normal/sync/karesz.js'),
     require('./commands/normal/sync/joe mama.js'),
@@ -20,17 +20,7 @@ client.once('ready', () => {
 });
 
 client.on('message', async message => {
-    var text = message.content.toLowerCase()
-    if (message.author.bot) return;
-    if (text == `${config.prefix}unpause`) {
-        pause = false;
-        message.channel.send("Unpaused instance this instance of the bot")
-    }
-    if (pause) return;
-    if (text == `${config.prefix}pause`) {
-        pause = true;
-        message.channel.send("Paused instance this instance of the bot")
-    }
+    if (pauseUnpauseCommand(message)) return;
     normalCommands.forEach(comm => comm(message))
     normalAwaitCommands.forEach(async awComm => await awComm(message))
     
